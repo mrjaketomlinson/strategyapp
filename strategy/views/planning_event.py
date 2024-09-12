0e5a54caeb2fc6a92e2204829c50fed6090f1638
Jacob Tomlinson <jacob.tomlinson21@gmail.com>
2024-09-11 21:31:58 -0500

    Complete planning event CRUD; add planning event to navbar

--- strategy/views/planning_event.py
+++ strategy/views/planning_event.py
@@ -20,10 +20,6 @@ def planning_event_all(request):
     ).order_by("name")
     context = {
         "planning_events": planning_events,
-        "breadcrumbs": [
-            {"title": "Admin", "url": reverse("account:admin")},
-            {"title": "Planning Events", "url": reverse("strategy:planning_event_all")},
-        ],
     }
     return render(request, "strategy/planning_event_all.html", context)
 
@@ -36,12 +32,16 @@ def planning_event_create(request):
         if form.is_valid():
             planning_event = form.save()
             messages.success(request, "Planning event created successfully!")
+            return redirect(
+                "strategy:planning_event_detail",
+                kwargs={"planning_event_id": planning_event.pk},
+            )
         else:
             for err, message in form.errors.items():
                 messages.error(
                     request, f'Field: {err}. Message: {"; ".join(m for m in message)}'
                 )
-        return redirect("strategy:planning_event_all")
+            return redirect("strategy:planning_event_create")
     else:
         initial = {
             "organization": request.user.organization,
@@ -50,19 +50,24 @@ def planning_event_create(request):
         }
         form = PlanningEventCreateForm(initial=initial)
         context = {
-            "body": render_to_string(
-                "base_form.html",
-                {
-                    "form": form,
-                    "url": reverse("strategy:planning_event_create"),
-                    "form_id": "planning-event-create-form",
-                    "button": "Create",
-                },
-                request=request,
-            ),
-            "title": "Create new planning event",
+            "form": form,
+            "url": reverse("strategy:planning_event_create"),
+            "form_id": "planning-event-create-form",
+            "button": "Create",
         }
-        return JsonResponse(context)
+        return render(request, "strategy/planning_event_create.html", context)
+
+
+@logged_in_user
+@require_GET
+def planning_event_detail(request, planning_event_id):
+    planning_event = get_object_or_404(
+        PlanningEvent, pk=planning_event_id, organization=request.user.organization
+    )
+    context = {
+        "event": planning_event
+    }
+    return render(request, "strategy/planning_event_detail.html", context)
 
 
 @logged_in_user
@@ -81,21 +86,41 @@ def planning_event_edit(request, planning_event_id):
                 messages.error(
                     request, f'Field: {err}. Message: {"; ".join(m for m in message)}'
                 )
-        return redirect("strategy:planning_event_all")
+        return redirect(
+            "strategy:planning_event_detail",
+            kwargs={"planning_event_id": planning_event.pk},
+        )
     else:
         initial = {"modified_by": request.user}
         form = PlanningEventEditForm(instance=planning_event, initial=initial)
         context = {
-            "body": render_to_string(
-                "base_form.html",
-                {
-                    "form": form,
-                    "url": reverse("strategy:planning_event_edit", kwargs={"planning_event_id": planning_event_id}),
-                    "form_id": "planning-event-edit-form",
-                    "button": "Update",
-                },
-                request=request,
+            "form": form,
+            "url": reverse(
+                "strategy:planning_event_edit",
+                kwargs={"planning_event_id": planning_event_id},
             ),
-            "title": "Update planning event",
+            "form_id": "planning-event-edit-form",
+            "button": "Update",
         }
-        return JsonResponse(context)
+        return render(request, "strategy/planning_event_edit.html", context)
+
+
+@logged_in_user
+@require_POST
+def planning_event_delete(request, planning_event_id):
+    try:
+        planning_event = get_object_or_404(
+            PlanningEvent,
+            pk=planning_event_id,
+            organization=request.user.organization,
+        )
+        planning_event.delete()
+        messages.success(request, "Planning event deleted successfully.")
+    except Exception as e:
+        messages.error(request, "There was an issue deleting that planning event.")
+        # TODO: proper logging
+        print(
+            f"planning_event_delete Exception. user: {request.user}, planning_event: {planning_event_id}.",
+            e,
+        )
+    return redirect("strategy:planning_event_all")
